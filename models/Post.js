6ec320189e337600c1ac245ec89e954518b44c20
Clayton Wang <claydwang@gmail.com>
2018-03-19 10:00:39 -0700

    added user and post model

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,18 @@
 // require mongoose
 // create schema
+const
+    mongoose = require("mongoose"),
+    commentSchema = new mongoose.Schema({
+        body: String
+    }),
+    postSchema = new mongoose.Schema({
+        body: {type: String, required: true},
+        comments:[commentSchema],
+        user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    }, {timestamps: true}),
+    
+    Post = mongoose.model("Post", postSchema)
 
+    module.exports = Post
 // create model using mongoose
 // export model
--- models/User.js
+++ models/User.js
@@ -1,5 +1,19 @@
 // require mongoose
 // create schema
+const 
+    mongoose = require("mongoose"),
+    userSchema = new mongoose.Schema({
+        email: {type: String, required: true, unique: true},
+        username: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        imageURL = {type: String}
+    }),
+    User = mongoose.model("User", userSchema)
+
+    // PASSPORT STUFF
+
+    module.exports = User
+
 
 // create model using mongoose
 // export model
