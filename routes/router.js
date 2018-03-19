bbac53a02cda72a26f8d4d2c6aef21c29446a9cd
Eli Meschures <meschures.eli@gmail.com>
2018-03-19 10:51:35 -0700

    set up routes for user and posts

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,33 @@
 // require express
 // create new router using express
 // require controller
+const
+    express = require('express'),
+    router = new express.Router(),
+    passport = require('passport'),
+    userController = require('../controllers/userController.js')
+    postController = require('../controllers/postController.js')
 
+    router.get('/', userController.index)
+    router.post('/users', userController.create)    
+    router.get('/user/:id/new', userController.new)
+    router.get('/user/:id/', userController.edit)
+    router.get('/user/:id', userController.show)
+    router.patch('/user/:id/', userController.update)
+    router.delete('/user/:id', userController.destroy)
+
+    router.get('/', postController.index)
+    router.post('/post/:id', postController.create) 
+    router.get('/post/:id/new', postController.new)
+    router.get('/user/:id/post/:id', postController.edit)
+    router.get('/post/:id', postController.show)
+    router.patch('/user/:id/post/:id', postController.update)
+    router.delete('/post/:id', postController.destroy)
 
 
 // export router
+module.exports = routers
+
+
+
+
