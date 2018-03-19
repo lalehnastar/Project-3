f843c9f7abfb70ef495c3ab5f1dbc7b15c47917c
Dario Miceli <dariomiceli@gmail.com>
2018-03-19 14:27:28 -0700

    cleaned up router and controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,54 +2,46 @@
 const Post = require('../models/Post.js')
 
 module.exports = {
-    //index
-index: (req, res) =>{
-    Post.find({}, (err, allDemPosts)=> {
-       res.json(allDemPosts)
-    })
 
-},
-//show
-show: (req, res) =>{
-    Post.findById(req.params.id, (err, thatPost) => {
+    index: (req, res) =>{
+        Post.find({}, (err, allDemPosts)=> {
+        res.json(allDemPosts)
+        })
+
+    },
+
+    show: (req, res) =>{
+        Post.findById(req.params.id, (err, thatPost) => {
+            if(err) return res.json({ success: false })
+        res.json(thatPost)
+        })
+    },
+    
+    new: (req, res) =>{
+    },
+
+    create: (req, res) =>{
+    
+        Post.create(req.body, (err, brandNewPost) => {
+        
+            if(err) return res.json({ success: false })
+            res.json({ success: true, message: "post created.", post: brandNewPost })
+        })
+    },
+    
+    edit: (req, res) =>{
+    },
+
+    update: (req, res) =>{
+        Post.findByIdAndUpdate(req.params.id, req.body, (err, updatedPost)=>{
+            res.json({ success: true, message: "post updated.", post: updatedPost})
+        })
+    },
+
+    destroy: (req, res) =>{
+    Post.findByIdAndRemove(req.params.id, (err) =>{
         if(err) return res.json({ success: false })
-       res.json(thatPost)
-    })
-   
-},
-//new 
-new: (req, res) =>{
- 
-},
-//create
-create: (req, res) =>{
-   
-    Post.create(req.body, (err, brandNewPost) => {
-     
-        if(err) return res.json({ success: false })
-        res.json({ success: true, message: "post created.", post: brandNewPost })
-    })
-
-
-  
-},
-//edit 
-edit: (req, res) =>{
-  
-},
-//update
-update: (req, res) =>{
-    Post.findByIdAndUpdate(req.params.id, req.body, (err, updatedPost)=>{
-        res.json({ success: true, message: "post updated.", post: updatedPost})
-    })
-},
-
-//destroy
-destroy: (req, res) =>{
-  Post.findByIdAndRemove(req.params.id, (err) =>{
-    if(err) return res.json({ success: false })
-    res.json({ success: true, message: "post deleted." })
-  } )
-
-}
+        res.json({ success: true, message: "post deleted." })
+        })
+    }
 } 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,113 +1,45 @@
-// require model
-
-const User = require('../models/User.js')
-
-
+const User = require('../models/User.js')   // require model
 
 module.exports = {
 
-        //index
-
-index: (req, res) =>{
-
+    index: (req, res) => {
         User.find({}, (err, allDemUsers)=> {
-
             res.json(allDemUsers)
+        })
+    },
 
-    })
-
-
-
-},
-
-//show
-
-show: (req, res) =>{
-
+    show: (req, res) => {
         User.findById(req.params.id, (err, thatUser) => {
-
             if(err) return res.json({ success: false })
+                res.json(thatUser)
+        })
+    },
 
-       res.json(thatUser)
+    new: (req, res) => {
+        res.render('../views/partials/sign-up.ejs')
+    },
 
-    })
+    create: (req, res) =>{
+        User.create(req.body, (err, brandNewUser) => {
+            if(err) return res.json({ success: false })
+            res.json({ success: true, message: "user created.", user: brandNewUser })
+        })
+    },
 
-   
+    edit: (req, res) =>{
 
-},
+    },
 
-
-
-//new 
-
-new: (req, res) =>{
-
-       
-
-},
-
-
-
-
-
-//create
-
-create: (req, res) =>{
-
-      
-
-    User.create(req.body, (err, brandNewUser) => {
-
-         
-
-        if(err) return res.json({ success: false })
-
-        res.json({ success: true, message: "user created.", user: brandNewUser })
-
-    })
-
-
-
-  
-
-},
-
-
-
-//edit 
-
-edit: (req, res) =>{
-
-       
-
-},
-
-//update
-
-update: (req, res) =>{
-
-        User.findByIdAndUpdate(req.params.id, req.body, (err, updatedUser)=>{
-
+    update: (req, res) =>{
+        User.findByIdAndUpdate(req.params.id, req.body, (err, updatedUser) => {
             res.json({ success: true, message: "user updated.", user: updatedUser})
+        })
+    },
 
-    })
-
-},
-
-
-
-//destroy
-
-destroy: (req, res) =>{
-
-      User.findByIdAndRemove(req.params.id, (err) =>{
-
-        if(err) return res.json({ success: false })
-
-    res.json({ success: true, message: "user deleted." })
-
-  } )
-
-}
-
+    destroy: (req, res) =>{
+        User.findByIdAndRemove(req.params.id, (err) =>{
+            if(err) return res.json({ success: false })
+            res.json({ success: true, message: "user deleted." })
+        })
+    }
 }
